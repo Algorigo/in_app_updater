4e0bb435be5a5314e61d53bc35f1f1594f551b8e
seunghwan <seosh817@naver.com>
2024-03-15 11:24:59 +0900

    in_app_updater_test 오버라이딩

--- test/in_app_updater_test.dart
+++ test/in_app_updater_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:in_app_updater/data/in_app_update_info.dart';
 import 'package:in_app_updater/in_app_updater.dart';
 import 'package:in_app_updater/in_app_updater_platform_interface.dart';
 import 'package:in_app_updater/in_app_updater_method_channel.dart';
@@ -10,6 +11,192 @@ class MockInAppUpdaterPlatform
 
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<InAppUpdateInfo> checkForUpdate() {
+    // TODO: implement checkForUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> checkUpdateAvailable() {
+    // TODO: implement checkUpdateAvailable
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> completeFlexibleUpdate() {
+    // TODO: implement completeFlexibleUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<InAppUpdateInfo> fakeCheckForUpdate() {
+    // TODO: implement fakeCheckForUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> fakeCheckUpdateAvailable() {
+    // TODO: implement fakeCheckUpdateAvailable
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeCompleteFlexibleUpdate() {
+    // TODO: implement fakeCompleteFlexibleUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeDownloadCompletes() {
+    // TODO: implement fakeDownloadCompletes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeDownloadFails() {
+    // TODO: implement fakeDownloadFails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeDownloadStarts() {
+    // TODO: implement fakeDownloadStarts
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> fakeGetTypeForUpdate() {
+    // TODO: implement fakeGetTypeForUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeInstallCompletes() {
+    // TODO: implement fakeInstallCompletes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeInstallFails() {
+    // TODO: implement fakeInstallFails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> fakeIsConfirmationDialogVisible() {
+    // TODO: implement fakeIsConfirmationDialogVisible
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> fakeIsImmediateFlowVisible() {
+    // TODO: implement fakeIsImmediateFlowVisible
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> fakeIsInstallSplashScreenVisible() {
+    // TODO: implement fakeIsInstallSplashScreenVisible
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream fakeObserveInAppUpdateInstallState() {
+    // TODO: implement fakeObserveInAppUpdateInstallState
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeSetBytesDownloaded(int bytes) {
+    // TODO: implement fakeSetBytesDownloaded
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeSetClientVersionStalenessDays(int days) {
+    // TODO: implement fakeSetClientVersionStalenessDays
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeSetInstallErrorCode(int errorCode) {
+    // TODO: implement fakeSetInstallErrorCode
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeSetTotalBytesToDownload(int bytes) {
+    // TODO: implement fakeSetTotalBytesToDownload
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeSetUpdateAvailable(int isAvailable) {
+    // TODO: implement fakeSetUpdateAvailable
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeSetUpdateNotAvailable() {
+    // TODO: implement fakeSetUpdateNotAvailable
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeSetUpdatePriority(int priority) {
+    // TODO: implement fakeSetUpdatePriority
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeStartUpdateFlexible() {
+    // TODO: implement fakeStartUpdateFlexible
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeStartUpdateImmediate() {
+    // TODO: implement fakeStartUpdateImmediate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> fakeTypeForUpdateInProgress() {
+    // TODO: implement fakeTypeForUpdateInProgress
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeUserAcceptsUpdate() {
+    // TODO: implement fakeUserAcceptsUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fakeUserRejectsUpdate() {
+    // TODO: implement fakeUserRejectsUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream observeInAppUpdateInstallState() {
+    // TODO: implement observeInAppUpdateInstallState
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> startUpdateFlexible() {
+    // TODO: implement startUpdateFlexible
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> startUpdateImmediate() {
+    // TODO: implement startUpdateImmediate
+    throw UnimplementedError();
+  }
 }
 
 void main() {
